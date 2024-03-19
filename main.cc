ee2e550f13eb29dc1f8837ea90e42e03e48bf9cd
SwordJoe <2410330450@qq.com>
2024-03-19 15:01:25 +0800

    add sleep

--- main.cc
+++ main.cc
@@ -1,11 +1,13 @@
 #include<iostream>
 #include<ctime>
-
+#include<unistd.h>
 int main()
 {
     srand(time(nullptr));
     while(1){
         std::cout << rand() % 1000 << std::endl;
+        std::cout << rand() % 100 << std::endl;
+        sleep(1);
     }
     return 0;
 }
